3d0d5d923cd0904a7fe5606d3809db1834e65eca
disma23196-lab <disma.23196@mhs.unesa.ac.id>
2026-04-05 10:25:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-BOT_TOKEN = "8796399908:AAHpT6F0J-NkVy1toWfe78w9OYHPJJxeZ6o"
+BOT_TOKEN = "8796956333:AAF_W7uaD68eROj1IYIlY51G479OwzheEUM"
 
-CHANNEL_USERNAME = "@menfessbksmpn54sby"
-MAIN_CHANNEL_ID = -1003666493378
-ADMIN_GROUP_ID = -1003415804436
+CHANNEL_USERNAME = "@menfesssmpn54sby"
+MAIN_CHANNEL_ID = -1003650282810
+ADMIN_GROUP_ID = -5182431876
 
-ADMIN_IDS = [8435116196]
+ADMIN_IDS = [8642114035]
 
 FORBIDDEN_WORDS = ["anjing", "babi", "goblok", "kontol", "memek", "jancok", "cok", "asu", "tempek","tolol", "bajiangan", "sialan", "peli", "nggatel", "coli", "colmek", "gendeng"]
